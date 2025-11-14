a8b71def99dfc3e9d6d0e1c05d98f7283d26e710
Fa'iq Hammam <hammamfaiq@protonmail.com>
2025-11-14 14:16:40 +0700

    add asignment

---
+++ projek/balikkalimat.cpp
@@ -0,0 +1,24 @@
+//contoh : Jaki Ganteng -> Ganteng Jaki menggunakan array  of char
+#include <iostream>
+using namespace std;
+
+int main() {
+    string kalimat;
+    cout << "Masukkan kalimat: ";
+    getline(cin, kalimat);
+    int panjang = kalimat.length();
+    int posisi_spasi = -1;
+    for (int i = 0; i < panjang; i++) {
+        if (kalimat[i] == ' ') {
+            posisi_spasi = i;
+            break;
+        }
+    }
+    if (posisi_spasi != -1) {
+        string pertama = kalimat.substr(0, posisi_spasi);
+        string kedua = kalimat.substr(posisi_spasi + 1);
+        string kalimat_balik = kedua + " " + pertama;
+        cout << "Kalimat setelah dibalik: " << kalimat_balik << endl;
+    } else {
+        cout << "Kalimat tidak mengandung spasi." << endl;}
+}
