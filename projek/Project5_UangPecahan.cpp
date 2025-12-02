1fd427606059c11fdd95b10ea8043b570488b4fb
Fa'iq Hammam <hammamfaiq@protonmail.com>
2025-12-02 12:09:13 +0700

    Update Project5_UangPecahan.cpp

--- projek/Project5_UangPecahan.cpp
+++ projek/Project5_UangPecahan.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
-void banyakPecahan(long uang, long pecahan[]);
 
-int main(){
-  long uang,pecahan[9] = {50000,20000,10000,5000,2000,1000,500,200,100};
-  cout << "Jumlah Uang Kembali : Rp "; cin >> uang;cout << "Pecahan uang kembali: " << endl;
-  banyakPecahan(uang,pecahan);
+void banyakPecahan(long uang, long pecahan[]) {
+    int lembar;
+
+    for (int i = 0; i < 9; i++) {
+        lembar = uang / pecahan[i];     // Hitung berapa lembar pecahan[i]
+        if (lembar > 0) {
+            cout << pecahan[i] << " : " << lembar << " lembar" << endl;
+        }
+        uang = uang % pecahan[i];       // Sisa uang setelah diambil pecahan itu
+    }
 }
 
-void banyakPecahan(long uang, long pecahan[]){
-  int i,lembar[9];
-  for(i=0;i<9;i++){
-    lembar[i] = uang/pecahan[i];
-    uang = uang - (lembar[i] * pecahan[i]);
-    cout << pecahan[i] << " : " << lembar[i] << " lembar" << endl;
-  }
+int main() {
+    long uang;
+    long pecahan[9] = {50000,20000,10000,5000,2000,1000,500,200,100};
+
+    cout << "Jumlah Uang Kembali : Rp ";
+    cin >> uang;
+
+    cout << "\nPecahan uang kembali:" << endl;
+    banyakPecahan(uang, pecahan);
+
+    return 0;
 }
