aecc5db8b9f0d2a492c4e5edcd55852cc15345c4
Fa'iq Hammam <hammamfaiq@protonmail.com>
2025-12-02 12:12:56 +0700

    Clean up formatting in uang pecahan function
    Removed unnecessary comments and adjusted whitespace in the banyakPecahan function for improved readability.

--- projek/Project5_UangPecahan.cpp
+++ projek/Project5_UangPecahan.cpp
@@ -5,14 +5,14 @@ void banyakPecahan(long uang, long pecahan[]) {
     int lembar;
 
     for (int i = 0; i < 9; i++) {
-        lembar = uang / pecahan[i];     // Hitung berapa lembar pecahan[i]
+        lembar = uang / pecahan[i];     
         if (lembar > 0) {
             cout << pecahan[i] << " : " << lembar << " lembar" << endl;
         }
-        uang = uang % pecahan[i];       // Sisa uang setelah diambil pecahan itu
+        uang = uang % pecahan[i];      
     }
 }
-
+`
 int main() {
     long uang;
     long pecahan[9] = {50000,20000,10000,5000,2000,1000,500,200,100};
