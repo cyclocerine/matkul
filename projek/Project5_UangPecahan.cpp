7fd33f554e59afab685b2a58cfa1288d1c893120
Fa'iq Hammam <hammamfaiq@protonmail.com>
2025-12-02 12:22:24 +0700

    Update Project5_UangPecahan.cpp

--- projek/Project5_UangPecahan.cpp
+++ projek/Project5_UangPecahan.cpp
@@ -12,7 +12,6 @@ void banyakPecahan(long uang, long pecahan[]) {
         uang = uang % pecahan[i];      
     }
 }
-`
 int main() {
     long uang;
     long pecahan[9] = {50000,20000,10000,5000,2000,1000,500,200,100};
